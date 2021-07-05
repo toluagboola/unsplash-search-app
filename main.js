fadf1c309c1598c1d2d8e2dffe89a40b8eb6d568
Toluwanimi Agboola <toluwanimiagboola@gmail.com>
2021-07-05 10:50:37 +0100

    Update comments and styles

--- main.js
+++ main.js
@@ -1,60 +1,65 @@
-const form = document.querySelector('.js-form');
-form.addEventListener('submit', handleSubmit);
-const nextBtn = document.querySelector('.js-next');
-const prevBtn = document.querySelector('.js-prev');
-let resultStats = document.querySelector('.js-result-stats');
-const spinner = document.querySelector('.js-spinner');
+const form = document.querySelector(".js-form");
+form.addEventListener("submit", handleSubmit);
+const nextBtn = document.querySelector(".js-next");
+const prevBtn = document.querySelector(".js-prev");
+let resultStats = document.querySelector(".js-result-stats");
+const spinner = document.querySelector(".js-spinner");
 let totalResults;
 let currentPage = 1;
 let searchQuery;
 
 const apiKey = "euralEva9ogeSkcZJTx5DlEEn9zx_-BeiwZg_mUXVqY";
 
-nextBtn.addEventListener('click', () => {
+// Page button click handler
+nextBtn.addEventListener("click", () => {
 	currentPage += 1;
 	fetchResults(searchQuery);
 });
 
-prevBtn.addEventListener('click', () => {
+prevBtn.addEventListener("click", () => {
 	currentPage -= 1;
 	fetchResults(searchQuery);
 });
 
+// Hide or show previous and next page buttons
 function pagination(totalPages) {
-	nextBtn.classList.remove('hidden');
+	nextBtn.classList.remove("hidden");
 	if (currentPage >= totalPages) {
-		nextBtn.classList.add('hidden');
+		nextBtn.classList.add("hidden");
 	}
 
-	prevBtn.classList.add('hidden');
+	prevBtn.classList.add("hidden");
 	if (currentPage !== 1) {
-		prevBtn.classList.remove('hidden');
+		prevBtn.classList.remove("hidden");
 	}
 }
 
+// Fetch results
 async function fetchResults(searchQuery) {
-	spinner.classList.remove('hidden');
+	spinner.classList.remove("hidden");
 	try {
 		const results = await searchUnsplash(searchQuery);
 		pagination(results.total_pages);
-		console.log(results);
+		spinner.classList.add("hidden");
 		displayResults(results);
-	} catch(err) {
-		console.log(err);
-		alert('Failed to search Unsplash');
+	} catch (err) {
+		alert("Failed to search Unsplash.");
 	}
-} 
+}
 
+// Pass search query to function to be fetched
 function handleSubmit(event) {
 	event.preventDefault();
 	currentPage = 1;
-	const inputValue = document.querySelector('.js-search-input').value;
+
+	const inputValue = document.querySelector(".js-search-input").value;
 	searchQuery = inputValue.trim();
-	console.log(searchQuery);
-	spinner.classList.add('hidden');
+
+	spinner.classList.add("hidden");
 	fetchResults(searchQuery);
 }
 
+// Search Unsplash based on search query
 async function searchUnsplash(searchQuery) {
 	const endpoint = `https://api.unsplash.com/search/photos?query=${searchQuery}&per_page=30&page=${currentPage}&client_id=${apiKey}`;
 	const response = await fetch(endpoint);
@@ -65,16 +70,17 @@ async function searchUnsplash(searchQuery) {
 	return json;
 }
 
+// Display feched images
 function displayResults(json) {
-	const searchResults = document.querySelector('.search-results');
-	searchResults.textContent = '';
-	json.results.forEach(result => {
+	const searchResults = document.querySelector(".search-results");
+	searchResults.textContent = "";
+	json.results.forEach((result) => {
 		const url = result.urls.small;
 		const unsplashLink = result.links.html;
 		const photographer = result.user.name;
 		const photographerPage = result.user.links.html;
 		searchResults.insertAdjacentHTML(
-			'beforeend',
+			"beforeend",
 			`<div>
 				<a href="${unsplashLink}" target="_blank">
 					<div class="result-item" style="background-image: url(${url});"></div>
@@ -83,8 +89,8 @@ function displayResults(json) {
 					<a href="${photographerPage}" target="_blank" style="color: black; text-decoration: none;">Photo by ${photographer}</a>
 				</p>
 			</div>`
-		);	
+		);
 	});
 	totalResults = json.total;
 	resultStats.textContent = `About ${totalResults} results found`;
-};
+}
